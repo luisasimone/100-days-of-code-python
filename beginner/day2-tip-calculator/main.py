49d6eb4c8a5cb39843c9701440d2b1ed1d0ba544
luisasimone <44923289+luisasimone@users.noreply.github.com>
2023-11-03 20:31:30 +0100

    Day 2

--- beginner/day2-tip-calculator/main.py
+++ beginner/day2-tip-calculator/main.py
@@ -2,6 +2,5 @@ tot = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people? "))
 
-tot_with_tip = tip / 100 * tot + tot
-to_pay = tot_with_tip / people
+to_pay = (tip / 100 * tot + tot) / people
 print(f"Each person should pay: ${round(to_pay, 2)}")
